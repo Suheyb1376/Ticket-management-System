bf091d3108fd2b79675eeb5dd4d05e38d084dafa
adnankahiye <adnankahiy@gmail.com>
2024-10-30 09:23:13 -0700

    Display Ticket Data

--- Controllers/TablesController.cs
+++ Controllers/TablesController.cs
@@ -1,10 +1,23 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AspnetCoreMvcFull.Models;
+using Microsoft.EntityFrameworkCore;
+using AspnetCoreMvcFull.Data;
 
 namespace AspnetCoreMvcFull.Controllers;
 
 public class TablesController : Controller
 {
-  public IActionResult Basic() => View();
+    private readonly ApplicationContext _context;
+    private object awaite;
+
+    public TablesController(ApplicationContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> Basic()
+    {
+        var Ticket = await _context.Tickets.ToListAsync();
+        return View(Ticket);
+    }
 }
--- obj/Debug/net8.0/AspnetCoreMvcFull.AssemblyInfo.cs
+++ obj/Debug/net8.0/AspnetCoreMvcFull.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6f5c290bb88b813ffdca33e570d628ffd28686")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437e116551b8850cc34d814aa29ff9b0a25e27c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
