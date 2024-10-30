61eef84c573fb521a6451867baa7a68600a48ee5
adnankahiye <adnankahiy@gmail.com>
2024-10-30 03:06:36 -0700

    New Form

---
+++ Controllers/TicketTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreMvcFull.Controllers
+{
+    public class TicketTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
